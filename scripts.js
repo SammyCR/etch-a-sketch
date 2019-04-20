6306b9979fe137d54e3db74555fd83f4ffebc4cc
Sammy Conrad-Rooney <samuelconradrooney@outlook.com>
2019-04-20 12:43:44 -0400

    Changed JS function so grid can have any side length

--- scripts.js
+++ scripts.js
@@ -1,14 +1,14 @@
-function create16Grid(){
-    for(let i=0; i<(256); i++){
+function createGrid(sideL){
+    let gridElementSide = 400/sideL;
+    for(let i=0; i<(sideL**2); i++){
         let gridE = document.createElement('div');
-        let gridElementSide = 400/16;
         gridE.style.cssText = `height: ${gridElementSide-2}px; width: ${gridElementSide-2}px; border: 1px solid black;`;
         gridE.classList.add('gridElement');
         let container = document.querySelector("#gridContainer");
         container.appendChild(gridE);
     }
     let container = document.querySelector("#gridContainer");
-    container.style.cssText = `grid-template-columns: repeat(16, 25px); grid-template-rows: repeat(16, 25px);`
+    container.style.cssText = `grid-template-columns: repeat(${sideL}, ${gridElementSide}px); grid-template-rows: repeat(${sideL}, ${gridElementSide}px);`
 }
 function clearGrid(){
     const gridElements = document.querySelectorAll(".gridElement");
@@ -16,7 +16,7 @@ function clearGrid(){
         element.classList.remove('color');
     })
 }
-create16Grid();
+createGrid(16);
 
 const gridElements = document.querySelectorAll(".gridElement");
 gridElements.forEach((element) => {
